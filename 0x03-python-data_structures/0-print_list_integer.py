17574830719d8db5de2b8b829e8ea4facaff3267
miss-mimie <mariam.musah55@gmial.com>
2024-09-19 13:23:23 -0700

    Python data structure

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def def print_list_integer(my_list=[]):
+    """
+    Prints the elements(integers) in a list
+
+    Args:
+    my_list: the list containing the integers to print
+    """
+
+    for element in my_list:
+        print("{:d}",format(element))
