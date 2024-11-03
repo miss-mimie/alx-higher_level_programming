beeffb8aa1b05f411fc1aadc4362a06f71473561
miss-mimie <mariam.musah55@gmial.com>
2024-11-03 15:53:53 -0800

    Python data structurs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 
-def element_at(my_list, idx: int) -> int:
+def element_at(my_list: list, idx: int) -> int:
     """
     Returns the element at a particular index in a list in C-style
 
     Args:
-    my_list (list): the list to search
-    idx (int): the index
+        my_list (list): the list to search
+        idx (int): the index
 
     Returns:
-    int: the value of the element if found, else None
+        int: the value of the element if found, else None
     """
     # handle invalid index ranges - negatives not accepted as well
     if 0 > idx >= len(my_list):
@@ -20,4 +20,4 @@ def element_at(my_list, idx: int) -> int:
         if i == idx:
             return data
 
-        return None #element not found
+    return None  # element not found
