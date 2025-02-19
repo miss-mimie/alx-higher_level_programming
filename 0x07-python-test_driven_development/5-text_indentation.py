e3918c634a23efbbceefd1039aa42a1f297f2091
miss-mimie <mariam.musah55@gmial.com>
2025-02-19 04:40:39 -0800

    PY test driven development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-This module provides a  function that prints a text with 2 new lines after each of
-these characters: ., ? and :
+This module provides a  function that prints a text with 2
+new lines after each of these characters: ., ? and :
 """
 
 
